fcf35e33ae75b64145eb62a1895c3cea4ca4c3af
SergiyOl <ser4002@gmail.com>
2025-03-24 14:14:42 +0200

    Created student repository

---
+++ testspr/src/main/java/com/testspring/ukd/testspr/repository/StudentRepository.java
@@ -0,0 +1,20 @@
+package com.testspring.ukd.testspr.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.testspring.ukd.testspr.model.*;
+
+public class StudentRepository {
+
+    private final List<Student> students = new ArrayList<>();
+
+    public StudentRepository() {
+        students.add(new Student(0, "Serhii", 16));
+        students.add(new Student(1, "John", 19));
+        students.add(new Student(2, "Kate", 17));
+    }
+
+    public List<Student> getAllUsers() {
+        return students;
+    }
+}
