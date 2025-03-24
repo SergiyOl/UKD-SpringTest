7aaa80f233175b951f2c26be2db5b0d4f895819c
SergiyOl <ser4002@gmail.com>
2025-03-24 12:24:21 +0200

    Replaced manual users creation with UserService

--- testspr/src/main/java/com/testspring/ukd/testspr/controller/Users.java
+++ testspr/src/main/java/com/testspring/ukd/testspr/controller/Users.java
@@ -2,23 +2,21 @@ package com.testspring.ukd.testspr.controller;
 
 // import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
-import java.util.Arrays;
+import com.testspring.ukd.testspr.service.UserService;
+
+import org.springframework.ui.Model;
 
 import org.springframework.stereotype.Controller;;
 
 @Controller
 public class Users {
 
+    private final UserService userService = new UserService();
+
     @GetMapping("/users")
     public String indexPage(Model model) {
-        model.addAttribute("users", Arrays.asList(
-                new User(0, "Serhii", "myemail@gmail.com"),
-                new User(1, "John", "hisemail@gmail.com"),
-                new User(2, "Kate", "heremail@gmail.com")
-
-        ));
+        model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
 
