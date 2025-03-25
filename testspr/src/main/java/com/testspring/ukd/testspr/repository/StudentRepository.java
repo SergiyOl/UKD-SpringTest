0a0f294de88219d84d79a3c432f2f5e6758c49fa
SergiyOl <ser4002@gmail.com>
2025-03-25 18:30:02 +0200

    Added CRUD operations to StudentRepository

--- testspr/src/main/java/com/testspring/ukd/testspr/repository/StudentRepository.java
+++ testspr/src/main/java/com/testspring/ukd/testspr/repository/StudentRepository.java
@@ -6,7 +6,7 @@ import com.testspring.ukd.testspr.model.*;
 
 public class StudentRepository {
 
-    private final List<Student> students = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
 
     public StudentRepository() {
         students.add(new Student(0, "Serhii", 16));
@@ -16,5 +16,39 @@ public class StudentRepository {
 
     public List<Student> getAllStudents() {
         return students;
+    }
+
+    public void addStudent(int id, String name, int age) {
+        students.add(new Student(id, name, age));
+    }
+
+    public List<Student> findStudentById(int id) {
+        List<Student> foundStudent = new ArrayList<>();
+        for (Student student : students) {
+            if (student.getId() == id) {
+
+            }
+        }
+        return foundStudent;
+    }
+
+    public void deleteStudentById(int id) {
+        for (Student toDelete : findStudentById(id)) {
+            for (Student student : students) {
+                if (student.getId() == toDelete.getId()) {
+                    students.remove(student);
+                }
+            }
+        }
+    }
+
+    public void updateStudent(int id, String name, int age) {
+        for (Student toUpdate : findStudentById(id)) {
+            for (Student student : students) {
+                if (student.getId() == toUpdate.getId()) {
+                    student = new Student(id, name, age);
+                }
+            }
+        }
     }
 }
